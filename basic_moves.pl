a818be4e9afd5404de65732b0c99eec26017d4e3
unknown <dfournier@IF501-204-17.insa-lyon.fr>
2014-10-06 11:44:03 +0200

    distibuting seed in own field

---
+++ basic_moves.pl
@@ -0,0 +1,18 @@
+a([4,4,4,4,4,0]).
+b([4,4,4,4,4,0]).
+
+
+
+
+replace([_|T], 0, X, [X|T]).
+replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
+replace(L, _, _, L).
+
+doReplace(L,I,X,NL) :- replace(L,I,X,NL),!.
+
+/* Distribue les graines à partir de la position pos (on met une graine dans pos)*/
+distribuer(0, _, Y, _,Y).
+distribuer(Seed, Pos, Joueur, Adversaire,Y) :- nth0(Pos, Joueur, X),X1 is X+1, doReplace(Joueur,Pos,X1,NJoueur),NewSeed is Seed-1, NewPos is Pos+1,distribuer(NewSeed, NewPos, NJoueur, Adversaire,Y).
+
+
+coup(Position, Joueur, Adversaire):- nth0(Position, Joueur, X).
