f9118eb2888ee3d1d79552dd7296bfe62f668220
Donovan <donovan.fournier@insa-lyon.fr>
2014-10-13 11:54:09 +0200

    Update basic_moves

--- basic_moves.pl
+++ basic_moves.pl
@@ -21,7 +21,9 @@ move(Pos,Player, Opponent_field,NPlayer,NOpponent):-
 	Seed>0,
 	PosDis is Pos+1, 
 	replace_wrapper(Player,Pos, 0,Temp_player),
-	distribute_wrapper(Seed,PosDis,Temp_player, Opponent_field, NPlayer, NOpponent).
+	distribute_wrapper(Seed,PosDis,Temp_player, Opponent_field, Temp_bis_player, Temp_opponent),
+	Last_hole is Seed + Pos,
+	pick_seed(Last_hole, Temp_bis_player, Temp_opponent, NPlayer, NOpponent).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PRIVATE
 
@@ -70,7 +72,8 @@ distribute(0, _,NPlayer,NOpponent,NPlayer,NOpponent).
 %end in your own field
 distribute(Seed, Pos, Player_field, Opponent_field,NPlayer, NOpponent) :- 
 	X is 7-Pos, 
-	Seed<X,distribute_in_field(Seed, Pos, Player_field,NPlayer), 
+	Seed<X,
+	distribute_in_field(Seed, Pos, Player_field,NPlayer), 
 	copy(Opponent_field,NOpponent).
 %end in the opponent's field once
 distribute(Seed, Pos, Player_field, Opponent_field,NPlayer, NOpponent) :- 
@@ -84,7 +87,7 @@ distribute(Seed, Pos, Player_field, Opponent_field,NPlayer, NOpponent) :-
 distribute(Seed, Pos, Player_field, Opponent_field,NPlayer,  NOpponent) :- 
 	PSeed is 6-Pos, 
 	OSeed is 11-Pos-PSeed, 
-	Rest is Seed-PSeed-OSeed, 
+	Rest is Seed-PSeed-OSeed,
 	distribute_in_field(PSeed, Pos, Player_field,Temp_player),
 	distribute_in_field(OSeed, 0, Opponent_field,Temp_opponent), 
 	distribute(Rest, 0,Temp_player,Temp_opponent, NPlayer, NOpponent).
@@ -94,3 +97,20 @@ distribute(Seed, Pos, Player_field, Opponent_field,NPlayer,  NOpponent) :-
 %% ==============================================================================
 distribute_wrapper(Seed, Pos, Player_field, Opponent_field,NPlayer,NOpponent) :- distribute(Seed, Pos,  Player_field, Opponent_field,NPlayer,NOpponent), !.
 
+% ==============================================================================
+% Pick the seeds in the last hole and his previous holes after distributing if there are less than 3 seeds in each hole
+%% ==============================================================================
+pick_seed(Last_hole, Player_field, Opponent_field,NPlayer,NOpponent) :- 
+	Last_hole > 5,
+	Last_hole < 11,
+	Last_hole_index is Last_hole-6,
+	nth0(Last_hole_index, Opponent_field, Seed),
+	Seed < 4,
+	replace_wrapper(Opponent_field,Last_hole_index, 0,Temp_opponent),
+	nth0(5, Player_field, Old_Store_Value),
+	New_Store_Value is Seed + Old_Store_Value,
+	replace_wrapper(Player_field, 5, New_Store_Value, Temp_player),
+	New_last_hole is Last_hole - 1,
+	pick_seed(New_last_hole, Temp_player, Temp_opponent,NPlayer,NOpponent).
+
+pick_seed(_, Player_field, Opponent_field, Player_field, Opponent_field).
--- most_seed_player.pl
+++ most_seed_player.pl
@@ -4,5 +4,7 @@
 
 most_seed_player(PlayerId, Player_field, Opponent_field,NPlayer, NOpponent,Best_Index):- gain_par_coup(Player_field, Opponent_field, List_Moves, 0), max_member(Max_Member, List_Moves), nth0(Best_Index, List_Moves, Max_Member), move( Best_Index, Player_field, Opponent_field,NPlayer, NOpponent).
 
+
 gain_par_coup(_,_,_,5).
 gain_par_coup( J_avant,Adv_avant, [G|Liste_G], X) :- move( X, J_avant, Adv_avant, J_apres, _), nth0(5, J_apres, G), X1 is X+1, gain_par_coup( J_avant,Adv_avant, Liste_G, X1). /* Appel récursif à vérifier */
+gain_par_coup( J_avant,Adv_avant, [G|Liste_G], X) :- nth0(5, J_avant, G), X1 is X+1, gain_par_coup( J_avant,Adv_avant, Liste_G, X1).
