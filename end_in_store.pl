2ac6e970a0c76174538d3b5032d6bbcbcc4698b4
quentinDupont <quentin.dupont.45370@gmail.com>
2014-10-13 09:59:40 +0200

    Strategy : Try to end in store
    If cant finish in store => random :dancer:

---
+++ end_in_store.pl
@@ -0,0 +1,9 @@
+/* Essayer de finir dans le store */
+
+:- consult('basic_moves').
+:- use_module(library(lists)).
+
+ends_in_store(J_avant, I) :- nth0(I, J_avant, Nb_seeds),I<5 , Nb_seeds is 5-I.
+list_end_in_store(J_avant,L) :- bagof(C, ends_in_store(J_avant,C), L).
+pos_to_play(J_avant, P) :- (list_end_in_store(J_avant,L)) -> (max_list(P,L));(P is random(5)).
+play(J_avant, Adv_avant, J_apres, Adv_apres) :- pos_to_play(J_avant, Pos), move(Pos, J_avant, Adv_avant, J_apres, Adv_apres).
